4f078120c92b75f976238bd886b2898341f16e2b
delanoholanda <dellanomaia@gmail.com>
2023-04-04 12:08:42 -0300

    Alteração da Porta do docker

--- confConteiner.sh
+++ confConteiner.sh
@@ -1,4 +1,4 @@
 docker rm -f pdftocsv &&
 docker build -t pdftocsv:latest . &&
-docker run -d -p 5000:5000 --name=pdftocsv pdftocsv  &&
+docker run -d -p 8091:5000 --name=pdftocsv pdftocsv  &&
 docker logs --follow pdftocsv
